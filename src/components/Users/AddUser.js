065a06b492a386f561d5740d37a8cf4a2f7f1fef
savino <savino.zippone.ecom@gmail.com>
2021-11-09 18:25:34 +0100

    add modal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,32 +2,38 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
-import classes from './AddUser.models.css';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
+    const errorMessage = {
+        title: 'Errore durante il salvataggio',
+        message: 'Il form contiene errori.'
+    }
+
     const [username, setUsername] = useState('');
     const [age, setAge] = useState();
-    const [invalidForm, setInvalidForm] = useState(false);
+    const [error, setError] = useState('');
 
     const usernamaChangeHandler = (e) => {
         setUsername(e.target.value);
-        setInvalidForm(false);
+        setError(false);
     }
 
     const ageChangeHandler = (e) => {
         setAge(e.target.value);
-        setInvalidForm(false);
+        setError(false);
     }
 
     const addUserHandler = (e) => {
         e.preventDefault();
         if (username.trim().length === 0 || age.trim().length === 0) {
-            setInvalidForm(true);
+            setError(errorMessage);
             return;
         }
         if (+age < 1) {
-            setInvalidForm(true);
+            setError(errorMessage);
             return;
         }
 
@@ -35,30 +41,42 @@ const AddUser = (props) => {
         setAge('');
 
         props.onAddUser(username, age);
-        setInvalidForm(false);
+        setError(false);
+    }
+
+    const errorHandler = () => {
+        setError(false);
     }
 
     return (
-        <Card>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    type="text"
-                    value={username}
-                    onChange={usernamaChangeHandler}
+        <div>
+            {error && (
+                <ErrorModal
+                    title={error.title}
+                    message={error.message}
+                    onConfirm={errorHandler}
                 />
-                <label htmlFor="age">Età (in anni)</label>
-                <input
-                    id="age"
-                    type="number"
-                    value={age}
-                    onChange={ageChangeHandler}
-                />
-                <Button type="submit">Aggiungi Utente</Button>
-                {invalidForm && <span>Impossibile inviare il form</span>}
-            </form>
-        </Card>
+            )}
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        id="username"
+                        type="text"
+                        value={username}
+                        onChange={usernamaChangeHandler}
+                    />
+                    <label htmlFor="age">Età (in anni)</label>
+                    <input
+                        id="age"
+                        type="number"
+                        value={age}
+                        onChange={ageChangeHandler}
+                    />
+                    <Button type="submit">Aggiungi Utente</Button>
+                </form>
+            </Card>
+        </div >
     )
 }
 
