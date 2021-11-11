8a49578b47cfd2927fe33e3890af25ebc31bff1a
savino <savino.zippone.ecom@gmail.com>
2021-11-11 18:12:59 +0100

    add cart handler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartToggleState, setCartToggle] = useState(false);
+
+  const toggleShowCart = (state) => () => {
+    console.log(state);
+    setCartToggle(!state);
+  }
+
   return (
     <Fragment>
-      <Header />
-      <Cart />
+      {cartToggleState && <Cart onHideCart={toggleShowCart(cartToggleState)} />}
+      <Header onShowCart={toggleShowCart(cartToggleState)}/>
       <main>
         <Meals />
       </main>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,7 +5,7 @@ const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
       {[{ id: 'c1', name: 'sushi', price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
@@ -18,8 +18,8 @@ const Cart = (props) => {
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
+        <button className={classes.button} onClick={props.onHideCart}>Order</button>
       </div>
     </Modal>
   );
