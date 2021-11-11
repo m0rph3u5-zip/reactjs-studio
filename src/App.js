8948d8838e3c21c53f15317f3d1d9ebeb00a9751
savino <savino.zippone.ecom@gmail.com>
2021-11-11 17:15:07 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { Fragment } from "react";
+import { Fragment } from 'react';
 
-import Header from "./components/Layout/Header";
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header />
-      <h2>Let's get started!</h2>
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,23 +1,21 @@
-import React from "react";
+import { Fragment } from 'react';
 
-import HeaderCardButton from "./HeaderCardButton";
-import imageBackground from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
-    <React.Fragment>
+    <Fragment>
       <header className={classes.header}>
-        <h1>
-          React Menu APP
-        </h1>
-        <HeaderCardButton />
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
-        <img src={imageBackground} alt="immagine di sfondo" />
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
-    </React.Fragment>
-  )
-}
+    </Fragment>
+  );
+};
 
 export default Header;
