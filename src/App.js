80028c541fe4a59fede204da1e272008a77d8e77
savino <savino.zippone.ecom@gmail.com>
2021-11-11 14:55:54 +0100

    startup project

--- src/App.js
+++ src/App.js
@@ -1,40 +1,8 @@
-import React, { useState, useEffect } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import ReducerLogin from './components/Login/ReducerLogin';
-
 function App() {
-  const useRedurerLogin = true;
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    const userLoggerId = localStorage.getItem('userLoggedIn');
-    if (userLoggerId === 'true') {
-      setIsLoggedIn(true);
-    }
-  }, [isLoggedIn]);
-
-  const loginHandler = (email, password) => {
-    localStorage.setItem('userLoggedIn', true);
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('userLoggedIn');
-    setIsLoggedIn(false);
-  };
-
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && useRedurerLogin && <ReducerLogin onLogin={loginHandler}/>}
-        {!isLoggedIn && !useRedurerLogin && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
