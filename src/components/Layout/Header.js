7b70e521a83d08bdd0f77de9f6cc8141c431ebd7
m0rph3u5-zip <cia.waglio@yahoo.com>
2021-11-13 14:39:20 +0100

    fix project

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/meals.jpg';
+import mealsImage from '../../assets/bg.webp';
 import classes from './Header.module.css';
 
 const Header = (props) => {
