254d2bffc430b6ed45e24d386b47d29e2aaa4c5a
m0rph3u5-zip <cia.waglio@yahoo.com>
2021-11-20 16:33:14 +0100

    config project

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-import { useState } from 'react';
-
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
-import ButtonStatic from './components/Statless/ButtonStatic';
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
-
-  const onClickHandler = () => {
-    console.log('event button on click');
-  }
-  
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-      <ButtonStatic onClick={onClickHandler}>Tasto Demo</ButtonStatic>
-    </CartProvider>
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
   );
 }
 
