ed9b9a78265be0335fe5d725094e51d29a1b2492
m0rph3u5-zip <cia.waglio@yahoo.com>
2021-12-04 21:37:51 +0100

    create form validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,41 @@
+import { useState } from 'react';
+
 const SimpleInput = (props) => {
+  const [nameInput, setNameInput] = useState('');
+  const [nameIsValid, setNameIsValid] = useState(false)
+
+  const nameChangeHander = (e) => {
+    setNameInput(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    if (nameInput.trim.length <= 0) {
+      return;
+    }
+    console.log(nameInput);
+    setNameInput('');
+  };
+  const keyDownHandler = (e) => {
+    if (e.key === 'Enter') {
+      setNameInput(e.target.value);
+      formSubmitHandler(e);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <label htmlFor='name'>Il tuo nome</label>
+        <input
+          type='text'
+          id='name'
+          onChange={nameChangeHander}
+          onKeyDown={keyDownHandler}
+        />
       </div>
-      <div className="form-actions">
-        <button>Submit</button>
+      <div className='form-actions'>
+        <button>Invia</button>
       </div>
     </form>
   );
