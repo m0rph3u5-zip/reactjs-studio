0129b7fc4d69793cd1b7c619beb0cf749c36a654
savino <savino.zippone.ecom@gmail.com>
2021-12-16 16:06:21 +0100

    edit input hook

--- src/components/hooks/use-input.js
+++ src/components/hooks/use-input.js
@@ -1,18 +1,24 @@
 import { useReducer } from 'react';
 
+const ActionType = {
+  BLUR: 'BLUR',
+  INPUT: 'INPUT',
+  RESET: 'RESET',
+};
+
 const initialInputState = {
   value: '',
   isTouched: false,
 };
 
 const inputStateReducer = (state, action) => {
-  if (action.type === 'INPUT') {
+  if (action.type === ActionType.INPUT) {
     return { value: action.value, isTouched: state.isTouched };
   }
-  if (action.type === 'BLUR') {
-    return { isTouched: true, value: state.value };
+  if (action.type === ActionType.BLUR) {
+    return { value: state.value, isTouched: action.value };
   }
-  if (action.type === 'RESET') {
+  if (action.type === ActionType.RESET) {
     return initialInputState;
   }
   return initialInputState;
@@ -24,28 +30,29 @@ const useInput = (validateValue) => {
     initialInputState
   );
 
+  const valueChangeHander = (e) => {
+    dispatch({ type: ActionType.INPUT, value: e.target.value });
+  };
+
+  const inputBlurHandler = (e) => {
+    dispatch({ type: ActionType.BLUR, value: true });
+  };
+
   const valueIsValid = validateValue(inputState.value);
   const hasError = !valueIsValid && inputState.isTouched;
 
-  const valueChangeHandler = (event) => {
-    dispatch({ type: 'INPUT', value: event.target.value });
-  };
-
-  const inputBlurHandler = () => {
-    dispatch({ type: 'BLUR' });
-  };
-
   const reset = () => {
-    dispatch({ type: 'RESET' });
+    dispatch({ type: ActionType.RESET });
+    dispatch({ type: ActionType.RESET });
   };
 
   return {
     value: inputState.value,
     isValid: valueIsValid,
     hasError,
-    valueChangeHandler,
-    inputBlurHandler,
     reset,
+    valueChangeHander,
+    inputBlurHandler,
   };
 };
 
