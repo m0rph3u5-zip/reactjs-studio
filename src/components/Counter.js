9f3993e67e884ce2dcb180a04bbb0eb617cb02db
m0rph3u5-zip <cia.waglio@yahoo.com>
2021-12-18 23:00:24 +0100

    manipulate multiple state from redux store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,27 +3,38 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.show);
   const dispatch = useDispatch();
   const addHandler = () => {
     dispatch({
       type: 'ADD',
+      value: 1,
     });
   };
 
   const removeHandler = () => {
     dispatch({
       type: 'REMOVE',
+      value: 2,
+    });
+  };
+
+  const showCounterHandler = () => {
+    dispatch({
+      type: 'SHOW',
+      show: !show,
     });
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={addHandler}>Incrementa</button>
         <button onClick={removeHandler}>Decrementa</button>
       </div>
+      <button onClick={showCounterHandler}>Toggle Visibility</button>
     </main>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,32 @@
 import { createStore } from 'redux';
 
-const initState = { counter: 0 };
+const initState = {
+  counter: 0,
+  show: true,
+};
 const actionType = {
   ADD: 'ADD',
   REMOVE: 'REMOVE',
+  SHOW: 'SHOW',
 };
 
 const counterReducer = (state = initState, action) => {
   if (action.type === actionType.ADD) {
     return {
-      counter: state.counter + 1,
+      counter: state.counter + action.value,
+      show: state.show,
     };
   }
   if (action.type === actionType.REMOVE) {
     return {
-      counter: state.counter - 1,
+      counter: state.counter - action.value,
+      show: state.show,
+    };
+  }
+  if (action.type === actionType.SHOW) {
+    return {
+      counter: state.counter,
+      show: action.show,
     };
   }
   return state;
