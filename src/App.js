be80e1d62ba499ccdde7ee3ef72febb92b4a51f4
m0rph3u5-zip <cia.waglio@yahoo.com>
2021-12-24 12:59:28 +0100

    start prpject

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth';
-import Profile from './components/UserProfile';
-import { useSelector } from 'react-redux';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <>
-      <Header />
-      {!isAuth && <Auth />}
-      {isAuth && <Profile />}
-      <Counter />
-    </>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
