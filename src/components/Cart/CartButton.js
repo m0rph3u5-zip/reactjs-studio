27d001815379a9cf1ad470b51f4fc29af2f95919
m0rph3u5-zip <cia.waglio@yahoo.com>
2021-12-24 13:19:15 +0100

    edit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -3,7 +3,7 @@ import classes from './CartButton.module.css';
 const CartButton = (props) => {
   return (
     <button className={classes.button}>
-      <span>Il Tuo Carrello</span>
+      <span>Carrello</span>
       <span className={classes.badge}>1</span>
     </button>
   );
