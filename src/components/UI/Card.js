ff73296d9fc2c1624463fc608b394d5a600a977c
m0rph3u5-zip <cia.waglio@yahoo.com>
2021-12-24 13:05:14 +0100

    edit pj

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,8 +3,7 @@ import classes from './Card.module.css';
 const Card = (props) => {
   return (
     <section
-      className={`${classes.card} ${props.className ? props.className : ''}`}
-    >
+      className={`${classes.card} ${props.className ? props.className : ''}`}>
       {props.children}
     </section>
   );
