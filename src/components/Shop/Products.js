749ca1225e67ebb8a728b57b08fe9587a145a8ae
m0rph3u5-zip <cia.waglio@yahoo.com>
2021-12-24 19:53:46 +0100

    add dummy prducts

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,17 +1,40 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCT = [
+  {
+    id: 'm1',
+    title: 'Camicia',
+    price: 14.9,
+    description: 'maglia a manica corta',
+  },
+  {
+    id: 'm2',
+    title: 'T-Shirt',
+    price: 12.9,
+    description: 'maglia a manica corta',
+  },
+  {
+    id: 'm3',
+    title: 'Jeans',
+    price: 29.9,
+    description: 'maglia a manica corta',
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Prodotti</h2>
       <ul>
-        <ProductItem
-          id={1}
-          title='Test'
-          price={6}
-          description='descrizione del prodotto!'
-        />
+        {DUMMY_PRODUCT.map((item) => (
+          <ProductItem
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
       </ul>
     </section>
   );
