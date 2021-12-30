e3abb051f30f943c82064b0534498c1be6c1aef7
m0rph3u5-zip <cia.waglio@yahoo.com>
2021-12-30 16:41:19 +0100

    add sync data to cart db

--- src/App.js
+++ src/App.js
@@ -1,15 +1,103 @@
-import { useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+
+import { uiActions } from './store/ui-slice';
+
 function App() {
+  const firstUpdate = useRef(true);
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    if (!firstUpdate.current) {
+      const sendRequest = async () => {
+        const response = await fetch(
+          'https://react-studio-92313-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+          {
+            method: 'PUT',
+            body: JSON.stringify({
+              items: cart.items,
+              totalQuantity: cart.totalQuantity,
+            }),
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error('Impossiile caricare i dati sul server.');
+        }
+      };
+
+      sendRequest()
+        .then(() => {
+          dispatch(
+            uiActions.showNotification({
+              show: true,
+              status: 'success',
+              title: 'Carrello aggiornato',
+              message: 'il tuo carrelo è stato aggiornato',
+            })
+          );
+        })
+        .catch(() => {
+          dispatch(
+            uiActions.showNotification({
+              show: true,
+              status: 'error',
+              title: 'Errore',
+              message: 'Impossibile aggiornare il carrello!',
+            })
+          );
+        });
+    }
+    firstUpdate.current = false;
+
+    return () => {
+      console.log('reset');
+      setTimeout(() => {
+        dispatch(
+          uiActions.showNotification({
+            show: false,
+          })
+        );
+      }, 10000);
+    };
+  }, [cart, dispatch]);
+
+  useEffect(() => {
+    const getCartData = async () => {
+      const response = await fetch(
+        'https://react-studio-92313-default-rtdb.europe-west1.firebasedatabase.app/cart.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Impossiile caricare i dati sul server.');
+      }
+
+      getCartData();
+    };
+  }, []);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && notification.show && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        {!showCart && <Products />}
+      </Layout>
+    </>
   );
 }
 
