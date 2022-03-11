0fb964732daf2f946679b96298e64a7c2ca7c7b6
savino <savino.zippone.ecom@gmail.com>
2022-03-11 12:03:16 +0100

    react router section

--- src/App.js
+++ src/App.js
@@ -1,56 +1,8 @@
-import { useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import Notification from './components/UI/Notification';
-
-import { uiActions } from './store/ui-slice';
-import { GetCartData, PutCartData } from './store/cart-action';
-
 function App() {
-  const dispatch = useDispatch();
-  const firstUpdate = useRef(true);
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
-  const cart = useSelector((state) => state.cart);
-  const notification = useSelector((state) => state.ui.notification);
-
-  useEffect(() => {
-    if (!firstUpdate.current) {
-      dispatch(PutCartData(cart));
-    }
-    firstUpdate.current = false;
-
-    return () => {
-      setTimeout(() => {
-        dispatch(
-          uiActions.showNotification({
-            show: false,
-          })
-        );
-      }, 5000);
-    };
-  }, [cart, dispatch]);
-
-  useEffect(() => {
-    dispatch(GetCartData());
-  }, [dispatch]);
-
   return (
-    <>
-      {notification && notification.show && (
-        <Notification
-          status={notification.status}
-          title={notification.title}
-          message={notification.message}
-        />
-      )}
-      <Layout>
-        {showCart && <Cart />}
-        {!showCart && <Products />}
-      </Layout>
-    </>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
