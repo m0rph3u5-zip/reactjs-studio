7205659756dd1df47e31abe528807b429f82f94e
savino <savino.zippone.ecom@gmail.com>
2022-03-16 16:17:57 +0100

    add form validation

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,25 +1,51 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
+const emailValidation = (email) => email && ['@', '.'].includes();
+const passwordValidation = (password) => password && password.length > 8;
+
 const AuthForm = () => {
+  const userEmail = useRef();
+  const userPassword = useRef();
+
+  const [formInvalid, setFormValidation] = useState(false);
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const email = userEmail.current.value;
+    const password = userPassword.current.value;
+    if (!emailValidation(email) || !passwordValidation(password)) {
+      console.log('form is invalid');
+      setFormValidation(true);
+      console.log(formInvalid);
+      return;
+    }
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input
+            type='email'
+            id='email'
+            required
+            ref={userEmail}
+            className={`${formInvalid ? 'error' : ''}`}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={userPassword} />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
