eedbe3ee919733c8d3e3fb6b67f876ab927e5ef1
savino <savino.zippone.ecom@gmail.com>
2022-03-16 15:12:59 +0100

    create project

--- src/pages/AllQuotes.js
+++
@@ -1,9 +0,0 @@
-const AllQuote = () => {
-  return (
-    <div>
-      <h1>Elenco Prevetivi</h1>
-    </div>
-  );
-};
-
-export default AllQuote;
--- src/pages/NewQuote.js
+++
@@ -1,9 +0,0 @@
-const NewQuote = () => {
-  return (
-    <div>
-      <h1>Aggiungi Prevetivo</h1>
-    </div>
-  );
-};
-
-export default NewQuote;
